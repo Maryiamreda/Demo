f55f0b57bcede5ddd02ae745c55ffaf325d9b6ca
Maryiamreda <maryiamreda@gmail.com>
2024-07-26 03:16:55 +0300

    fix create student server issues

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const Students = require('./models/students')
 const skills = require('./models/Skills')
-// const Address = require('./models/Addresses')
+const Address = require('./models/Addresses')
 
 
 
@@ -63,24 +63,102 @@ app.get('/students/:FirstName', (req, res) => {
 })
 
 //CreateStudent
-
 app.post('/students/Create', async (req, res) => {
-    const student = new Students(req.body);
+    const { FirstName, LastName, Skills, Address: addressData } = req.body;
 
     try {
+        // Create new addresses if provided
+        const addressIds = await Promise.all(addressData?.map(async (address) => {
+            const newAddress = new Address(address);
+            const savedAddress = await newAddress.save();
+            return savedAddress._id;
+        }) || []);
+
+        // Create and save the student
+        const student = new Students({
+            FirstName,
+            LastName,
+            Address: addressIds,
+            Skills: Skills || [],
+        });
+
         const savedStudent = await student.save();
 
-        await skills.updateMany(
-            { '_id': { $in: savedStudent.Skills } },
-            { $push: { Students: savedStudent._id } }
-        );
+        // Update the skills collection
+        if (savedStudent.Skills.length > 0) {
+            await skills.updateMany(
+                { '_id': { $in: savedStudent.Skills } },
+                { $push: { Students: savedStudent._id } }
+            );
+        }
+
+        // Update the addresses collection
+        if (savedStudent.Address.length > 0) {
+            await Address.updateMany(
+                { '_id': { $in: savedStudent.Address } },
+                { $push: { Students: savedStudent._id } }
+            );
+        }
 
         res.json(savedStudent);
+        console.log(req.body);
+
     } catch (err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
+        console.error(err);
+        res.status(400).json({ error: err.message || 'An error occurred while creating the student.' });
     }
 });
+
+// app.post('/students/Create', async (req, res) => {
+
+//     const { FirstName, LastName, Skills, Adress: addressData } = req.body;
+
+//     try {
+//         // Create new addresses if provided
+//         if (addressData) {
+//             const addressIds = await Promise.all(addressData?.map(async (address) => {
+//                 const newAddress = new Address(address);
+//                 const savedAddress = await newAddress.save();
+//                 return savedAddress._id;
+//             }));
+//         }
+
+
+//         // Create and save the student
+//         const student = new Students({
+//             FirstName,
+//             LastName,
+//             Adress: addressIds,
+//             Skills,
+//         });
+
+//         const savedStudent = await student.save();
+
+//         // Update the skills collection
+//         if (savedStudent.Skills.length > 0) {
+//             await skills.updateMany(
+//                 { '_id': { $in: savedStudent.Skills } },
+//                 { $push: { Students: savedStudent._id } }
+//             );
+//         }
+
+//         // Update the addresses collection
+//         if (savedStudent.Adress.length > 0) {
+//             await Address.updateMany(
+//                 { '_id': { $in: savedStudent.Adress } },
+//                 { $push: { Students: savedStudent._id } }
+//             );
+//         }
+
+//         res.json(savedStudent);
+//         console.log(req.body);
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
+
 
 //EditStudent
 app.put('/students/Edit/:id', async (req, res) => {
